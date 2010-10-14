f64f21ad96711624ae2124fbb0768f44d415a79f
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2010-10-14 13:39:24 +0200

    add editor for byte[]

--- src/main/java/org/datacite/mds/web/MetadataController.java
+++ src/main/java/org/datacite/mds/web/MetadataController.java
@@ -1,12 +1,20 @@
 package org.datacite.mds.web;
 
-import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import org.datacite.mds.domain.Metadata;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.support.ByteArrayMultipartFileEditor;
 
 @RooWebScaffold(path = "metadatas", formBackingObject = Metadata.class, delete = false)
 @RequestMapping("/metadatas")
 @Controller
 public class MetadataController {
+
+    @InitBinder
+    protected void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(byte[].class, new ByteArrayMultipartFileEditor());
+    }
 }
