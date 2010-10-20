ec9e5b1e5ad9655a478f045a5c27b450edafc1df
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2010-10-20 14:40:47 +0200

    add util to check for specific constraint violation

--- src/main/java/org/datacite/mds/util/Utils.java
+++ src/main/java/org/datacite/mds/util/Utils.java
@@ -1,10 +1,12 @@
 package org.datacite.mds.util;
 
+import java.lang.annotation.Annotation;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 import javax.validation.ConstraintValidatorContext;
+import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
@@ -62,6 +64,29 @@ public class Utils {
         Validator validator = validatorFactory.getValidator();
         Set violations = validator.validateProperty(object, propertyName);
         return violations.isEmpty();
+    }
+
+    /**
+     * Check if a specific constraint annotation is violated
+     * 
+     * @param object
+     *            object to validate
+     * @param constraint
+     *            constraint annotation to be checked
+     * @return true if the given constraint is not violated
+     */
+    public static <T> boolean isConstraintValid(T object, Class<? extends Annotation> constraint) {
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+        Set<ConstraintViolation<T>> violations = validator.validate(object);
+
+        for (ConstraintViolation<T> violation : violations) {
+            if (violation.getConstraintDescriptor().getAnnotation().annotationType().equals(constraint)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public static void addConstraintViolation(ConstraintValidatorContext context, String message, String node) {
