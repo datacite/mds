3a8507ca778e6d664b76dd0d58f865c81abf64d8
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2010-10-22 13:13:36 +0200

    add 'switch to user' to ui

---
+++ src/main/java/org/datacite/mds/web/LogoutOrExitController.java
@@ -0,0 +1,32 @@
+package org.datacite.mds.web;
+
+import org.apache.log4j.Logger;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.switchuser.SwitchUserGrantedAuthority;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LogoutOrExitController {
+    Logger log = Logger.getLogger(LogoutOrExitController.class);
+    /**
+     * If the current user is logged in by "switch user", return to the original
+     * user. Otherwise do a normal logout.
+     */
+    @RequestMapping(value = "/resources/logout_or_exit", method = RequestMethod.GET)
+    public String logoutOrExit() {
+        Authentication currentAuth = SecurityContextHolder.getContext().getAuthentication();
+        for (GrantedAuthority auth : currentAuth.getAuthorities()) {
+            if (auth instanceof SwitchUserGrantedAuthority) {
+                SwitchUserGrantedAuthority su = (SwitchUserGrantedAuthority) auth;
+                log.debug("switch back to user " + su.getSource().getName());
+                return "redirect:/resources/j_spring_security_exit_user";
+            }
+        }
+        log.debug("normal logout");
+        return "redirect:/resources/j_spring_security_logout";
+    }
+}
