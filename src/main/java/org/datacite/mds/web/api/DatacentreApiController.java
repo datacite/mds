0dd9ab445992390f8fb8d70db8f1e97a1b897dc7
edzuk <admin@datacite.org.uk>
2010-11-04 20:56:29 +0000

    PUT implementation for datacentre API

--- src/main/java/org/datacite/mds/web/api/DatacentreApiController.java
+++ src/main/java/org/datacite/mds/web/api/DatacentreApiController.java
@@ -14,9 +14,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import java.util.Date;
 import org.datacite.mds.web.util.SecurityUtils;
 import org.datacite.mds.service.SecurityException;
+import javax.persistence.NoResultException;
 
 @RequestMapping("/*")
 @Controller
@@ -41,11 +43,13 @@ public class DatacentreApiController {
         } catch (SecurityException e) {
             return new ResponseEntity<String>(e.getMessage(), new HttpHeaders(), HttpStatus.FORBIDDEN);
         } catch (RuntimeException e) {
-        	return new ResponseEntity<String>("internal error - please contact admin", new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+        	return new ResponseEntity<String>("internal error - please contact admin", new HttpHeaders(), 
+                                              HttpStatus.INTERNAL_SERVER_ERROR);
         }
 	    
         if (!allocator.getSymbol().equals(datacentre.getAllocator().getSymbol())) {
-            return new ResponseEntity<String>("cannot request datacentre which belongs to another party", new HttpHeaders(), HttpStatus.FORBIDDEN);
+            return new ResponseEntity<String>("cannot request datacentre which belongs to another party", 
+                                              new HttpHeaders(), HttpStatus.FORBIDDEN);
         }
 
 	    return new ResponseEntity<Datacentre>(datacentre, new HttpHeaders(), HttpStatus.OK);	    
@@ -55,18 +59,71 @@ public class DatacentreApiController {
 	public ResponseEntity<? extends Object> createOrUpdate(@RequestBody @Valid Datacentre requestDatacentre,
 			@RequestParam(required = false) Boolean testMode) {
 
-        // TODO find logged in allocator
-        Allocator allocator = null;
-        
-        // TODO try to find requestDatacentre
+		if (testMode == null)
+			testMode = false;
+		log4j.debug("*****PUT datacentre " + requestDatacentre + " \ntestMode = " + testMode);
 
-        // if new, initialise it
-        requestDatacentre.setAllocator(allocator);
-        requestDatacentre.setCreated(new Date());
-	    requestDatacentre.setUpdated(new Date());
-        requestDatacentre.setRoleName("ROLE_DATACENTRE");
-	    
-        return null;
-	    
+        HttpHeaders headers = new HttpHeaders();
+
+        Allocator allocator;
+
+        try {
+            allocator = SecurityUtils.getAllocator();
+        } catch (SecurityException e) {
+            return new ResponseEntity<String>(e.getMessage(), headers, HttpStatus.FORBIDDEN);
+        } catch (RuntimeException e) {
+        	return new ResponseEntity<String>("internal error - please contact admin", 
+                                              headers, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if (!requestDatacentre.getSymbol().startsWith(allocator.getSymbol()))
+            return new ResponseEntity<String>("cannot PUT datacentre which belongs to another party", 
+                                              headers, HttpStatus.FORBIDDEN);
+
+        Datacentre datacentre = null;
+        try {
+            datacentre = Datacentre.findDatacentresBySymbolEquals(requestDatacentre.getSymbol()).getSingleResult();
+
+            if (!allocator.getSymbol().equals(datacentre.getAllocator().getSymbol()))
+                return new ResponseEntity<String>("cannot update datacentre which belongs to another party", 
+                                                  headers, HttpStatus.FORBIDDEN);
+
+            log4j.debug("*****PUT datacentre, found datacentre... updating ");
+
+        } catch (Exception e) {
+
+            log4j.debug("*****PUT datacentre: new datacentre");
+
+            requestDatacentre.setAllocator(allocator);
+            requestDatacentre.setRoleName("ROLE_DATACENTRE");
+            requestDatacentre.setUpdated(new Date());
+            requestDatacentre.setCreated(new Date());
+
+            if(!testMode)
+                requestDatacentre.persist();
+
+            headers.setContentType(MediaType.APPLICATION_XML);
+            return new ResponseEntity<Datacentre>(datacentre, headers, HttpStatus.CREATED);
+        }
+
+        datacentre.setAllocator(allocator);
+        datacentre.setRoleName("ROLE_DATACENTRE");
+        datacentre.setUpdated(new Date());
+        datacentre.setContactName(requestDatacentre.getContactName());
+        datacentre.setContactEmail(requestDatacentre.getContactEmail());
+        datacentre.setDoiQuotaAllowed(requestDatacentre.getDoiQuotaAllowed());
+        datacentre.setDoiQuotaUsed(requestDatacentre.getDoiQuotaUsed());
+        datacentre.setDomains(requestDatacentre.getDomains());
+        datacentre.setIsActive(requestDatacentre.getIsActive());
+        datacentre.setName(requestDatacentre.getName());
+        datacentre.setPassword(requestDatacentre.getPassword());
+        datacentre.getPrefixes().addAll(requestDatacentre.getPrefixes());
+        datacentre.setSymbol(requestDatacentre.getSymbol());
+
+        if(!testMode)
+            datacentre.merge();
+                
+        headers.setContentType(MediaType.APPLICATION_XML);
+        return new ResponseEntity<Datacentre>(datacentre, headers, HttpStatus.OK);
 	}
 }
