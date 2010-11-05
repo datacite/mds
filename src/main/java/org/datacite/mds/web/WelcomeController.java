fcda3f93a20087dbbb0e3eb58a3dbc82b9b2a47c
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2010-11-05 14:35:14 +0100

    add custom WelcomeController

---
+++ src/main/java/org/datacite/mds/web/WelcomeController.java
@@ -0,0 +1,27 @@
+package org.datacite.mds.web;
+
+import java.util.Collection;
+
+import org.datacite.mds.util.Utils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@RequestMapping("/")
+@Controller
+@SessionAttributes("symbols")
+public class WelcomeController {
+    @RequestMapping
+    public String welcome(Model model) {
+        return "index";
+    }
+
+    @ModelAttribute("symbols")
+    public Collection<String> populateSymbols() {
+        Collection<String> symbols = Utils.getAllSymbols();
+        symbols.add(""); // add empty string to disable symbol default selection
+        return symbols;
+    }
+}
