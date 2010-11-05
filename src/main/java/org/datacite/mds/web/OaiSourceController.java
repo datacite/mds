b3625290d1e239858dbb5a65da60b56c9740ff52
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2010-11-05 13:00:38 +0100

    add drop-down field for OaiSource.owner

--- src/main/java/org/datacite/mds/web/OaiSourceController.java
+++ src/main/java/org/datacite/mds/web/OaiSourceController.java
@@ -1,7 +1,15 @@
 package org.datacite.mds.web;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
+import org.datacite.mds.domain.Allocator;
+import org.datacite.mds.domain.Datacentre;
 import org.datacite.mds.domain.OaiSource;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 
@@ -9,4 +17,18 @@ import org.springframework.stereotype.Controller;
 @RequestMapping("/oaisources")
 @Controller
 public class OaiSourceController {
+    
+    @ModelAttribute("owners")
+    public Collection<String> populateOwners() {
+        SortedSet<String> owners = new TreeSet<String>();
+        owners.add(""); // add empty string to disable symbol default selection
+        for (Datacentre datacentre : Datacentre.findAllDatacentres()) {
+            owners.add(datacentre.getSymbol());
+        }
+        for (Allocator allocator: Allocator.findAllAllocators()) {
+            owners.add(allocator.getSymbol());
+        }
+        return owners;
+    }
+
 }
