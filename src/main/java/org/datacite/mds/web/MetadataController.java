547059ab23a00c7c5b1f74528f204c7016e3f580
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2010-12-03 16:59:27 +0100

    add link to show raw xml (closes #MDS-245)

--- src/main/java/org/datacite/mds/web/MetadataController.java
+++ src/main/java/org/datacite/mds/web/MetadataController.java
@@ -12,6 +12,10 @@ import org.datacite.mds.validation.constraints.impl.ValidXMLValidator;
 import org.datacite.mds.web.util.Converters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.support.GenericConversionService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -48,13 +52,13 @@ public class MetadataController {
         Dataset dataset = Dataset.findDataset(dataset_id);
         return Arrays.asList(dataset);
     }
-    
+
     @ModelAttribute("xsd")
     public String getXsd() {
-        //ugly; I was not able to use Springs @Value annotation
+        // ugly; I was not able to use Springs @Value annotation
         return new ValidXMLValidator().getXsd();
     }
-    
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public String show(@PathVariable("id") Long id, Model model) {
         Metadata metadata = Metadata.findMetadata(id);
@@ -69,5 +73,16 @@ public class MetadataController {
         model.addAttribute("prettyxml", prettyXml);
         model.addAttribute("itemId", id);
         return "metadatas/show";
+    }
+
+    @RequestMapping(value = "/{id}", params = "raw", method = RequestMethod.GET)
+    public ResponseEntity<? extends Object> show_raw(@PathVariable("id") Long id) {
+        HttpHeaders headers = new HttpHeaders();
+        Metadata metadata = Metadata.findMetadata(id);
+        if (metadata == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        headers.setContentType(MediaType.APPLICATION_XML);
+        return new ResponseEntity<Object>(metadata.getXml(), headers, HttpStatus.OK);
     }
 }
