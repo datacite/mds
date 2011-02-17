ba28e6da10596518e3c323674ed23da64c36e17b
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2011-02-17 16:59:52 +0100

    add class MailMessage

---
+++ src/main/java/org/datacite/mds/mail/MailMessage.java
@@ -0,0 +1,42 @@
+package org.datacite.mds.mail;
+
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
+import org.datacite.mds.domain.AllocatorOrDatacentre;
+import org.springframework.core.io.Resource;
+import org.springframework.mail.SimpleMailMessage;
+
+public class MailMessage extends SimpleMailMessage {
+    
+    public void setTo(AllocatorOrDatacentre user) {
+        this.setTo(user.getContactEmail());
+    }
+    
+    public void loadTemplate(Resource resource) {
+        String text;
+        try {
+            text = FileUtils.readFileToString(resource.getFile());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        loadTemplate(text);
+    }
+    
+    public void loadTemplate(String template) {
+        String[] split = template.split("\n", 2);
+        setSubject(split[0]);
+        setText(split[1]);
+    }
+    
+    public String getTemplate() {
+        return getSubject() + "\n" + getText();
+    }
+    
+    public void replacePlaceholer(String placeholder, String replacement) {
+        String template = getTemplate();
+        template = StringUtils.replace(template, "%" + placeholder + "%", replacement);
+        loadTemplate(template);
+    }
+}
---
+++ src/test/java/org/datacite/mds/mail/MailMessageTest.java
@@ -0,0 +1,37 @@
+package org.datacite.mds.mail;
+
+import static junit.framework.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MailMessageTest {
+    
+    MailMessage mail;
+    
+    @Before
+    public void init() {
+        mail = new MailMessage();
+    }
+    
+    @Test
+    public void loadTemplateAsString() {
+        String subject = "Subject line";
+        String text = "line on\n line two \n";
+        String template = subject + "\n" + text;
+        mail.loadTemplate(template);
+        assertEquals(subject, mail.getSubject());
+        assertEquals(text, mail.getText());
+    }
+    
+    @Test
+    public void replacePlaceholder() {
+        mail.setSubject("%aa% - %cc%");
+        mail.setText("%aa% - %bb%\n%aa%");
+        mail.replacePlaceholer("aa", "A");
+        mail.replacePlaceholer("bb", "B");
+        assertEquals("A - %cc%", mail.getSubject());
+        assertEquals("A - B\nA", mail.getText());
+    }
+
+}
