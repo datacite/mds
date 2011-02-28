e801f77e59c1573e0ce82816b2f98e1bfaafe9ca
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2011-02-28 16:37:59 +0100

    return Future<Object> for async mail method
    this allows capturing of exception by Future.get()

--- src/main/java/org/datacite/mds/service/impl/MailServiceImpl.java
+++ src/main/java/org/datacite/mds/service/impl/MailServiceImpl.java
@@ -1,5 +1,7 @@
 package org.datacite.mds.service.impl;
 
+import java.util.concurrent.Future;
+
 import org.apache.log4j.Logger;
 import org.datacite.mds.domain.AllocatorOrDatacentre;
 import org.datacite.mds.mail.MailMessage;
@@ -7,6 +9,7 @@ import org.datacite.mds.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -29,8 +32,9 @@ public class MailServiceImpl implements MailService {
     }
     
     @Async
-    public void sendAsync(MailMessage mail) {
+    public Future<Object> sendAsync(MailMessage mail) {
         send(mail);
+        return new AsyncResult<Object>(null);
     }
 
 }
