b9ff6265cc55da7cad8c153f42529b2bdb4adebd
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2011-02-28 17:52:55 +0100

    use NullSecurityContextRepository for API (closes #26)

---
+++ src/main/java/org/datacite/mds/web/api/NullSecurityContextRepository.java
@@ -0,0 +1,29 @@
+package org.datacite.mds.web.api;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpRequestResponseHolder;
+import org.springframework.security.web.context.SecurityContextRepository;
+
+/**
+ * @author Luke Taylor
+ * @since 3.1
+ */
+public final class NullSecurityContextRepository implements SecurityContextRepository {
+
+    public boolean containsContext(HttpServletRequest request) {
+        return false;
+    }
+
+    public SecurityContext loadContext(HttpRequestResponseHolder requestResponseHolder) {
+        return SecurityContextHolder.createEmptyContext();
+    }
+
+    public void saveContext(SecurityContext context, HttpServletRequest request, HttpServletResponse response) {
+    }
+
+
+}
