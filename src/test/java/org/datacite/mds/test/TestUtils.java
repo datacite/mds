291ae46c8888653f47204f37a2f65b986bc25867
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2011-03-04 15:41:48 +0100

    prevent hudson from treating util class as test

--- src/test/java/org/datacite/mds/test/TestUtils.java
+++ src/test/java/org/datacite/mds/test/TestUtils.java
@@ -14,8 +14,8 @@ import org.datacite.mds.domain.Prefix;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-public class TestUtils {
-
+public abstract class TestUtils {
+    
     public static void setUsernamePassword(String username, String password) {
         SecurityContextHolder.getContext().setAuthentication(
                 new UsernamePasswordAuthenticationToken(username, password));
