528eb856e041418d0950e42dc6489b020cc2b164
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2011-03-16 16:37:44 +0100

    userinfo: show datacentres for prefixes

--- src/main/java/org/datacite/mds/web/ui/controller/UserInfoController.java
+++ src/main/java/org/datacite/mds/web/ui/controller/UserInfoController.java
@@ -1,9 +1,16 @@
 package org.datacite.mds.web.ui.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.collections.ListUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.datacite.mds.domain.Allocator;
 import org.datacite.mds.domain.Datacentre;
 import org.datacite.mds.domain.Dataset;
+import org.datacite.mds.domain.Prefix;
 import org.datacite.mds.service.SecurityException;
 import org.datacite.mds.util.SecurityUtils;
 import org.springframework.stereotype.Controller;
@@ -35,6 +42,7 @@ public class UserInfoController {
     private void userinfoAllocator(Model model) throws SecurityException {
         Allocator allocator = SecurityUtils.getCurrentAllocator();
         addAllocatorToModel(allocator, model);
+        addPrefixesToModel(allocator, model);
     }
 
     private void addDatacentreToModel(Datacentre datacentre, Model model) {
@@ -47,4 +55,13 @@ public class UserInfoController {
         log.debug("userinfo for allocator '" + allocator.getSymbol() + "'");
         model.addAttribute("allocator", allocator);
     }
+    
+    private void addPrefixesToModel(Allocator allocator, Model model) {
+        Set<Prefix> prefixes = allocator.getPrefixes();
+        List<String> labels = new ArrayList<String>();
+        for (Prefix prefix : prefixes)
+            labels.add(prefix.getLabelWithDatacentres());
+        model.addAttribute("prefixes", labels);
+    }
+    
 }
