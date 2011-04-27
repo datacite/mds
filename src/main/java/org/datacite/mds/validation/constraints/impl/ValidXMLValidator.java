4330741f3e7e72aa7c50ee5beda7f415ace0249b
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2011-04-27 12:31:40 +0200

    use parsed schema location for validation

--- src/main/java/org/datacite/mds/validation/constraints/impl/ValidXMLValidator.java
+++ src/main/java/org/datacite/mds/validation/constraints/impl/ValidXMLValidator.java
@@ -5,28 +5,32 @@ import java.io.InputStream;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import javax.xml.XMLConstants;
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
 import org.apache.log4j.Logger;
+import org.datacite.mds.service.SchemaService;
 import org.datacite.mds.util.ValidationUtils;
 import org.datacite.mds.validation.constraints.ValidXML;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.stereotype.Component;
 import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
 
 @Configurable
+@Component
 public class ValidXMLValidator implements ConstraintValidator<ValidXML, byte[]> {
     Logger log = Logger.getLogger(ValidXMLValidator.class);
 
     String xsd;
 
     boolean enabled;
+    
+    @Autowired
+    SchemaService schemaService;
 
     public void initialize(ValidXML constraintAnnotation) {
         if (!constraintAnnotation.xsd().isEmpty()) {
@@ -36,11 +40,10 @@ public class ValidXMLValidator implements ConstraintValidator<ValidXML, byte[]> 
         log.debug("init: enabled=" + isEnabled());
     }
 
-    public boolean isValid(byte[] xmlBytes, ConstraintValidatorContext context) {
-        InputStream xml = new ByteArrayInputStream(xmlBytes);
+    public boolean isValid(byte[] xml, ConstraintValidatorContext context) {
         try {
             if (isEnabled()) {
-                checkValidity(xml, xsd);
+                checkValidity(xml);
             } else {
                 log.debug("validation skipped; checking only for well-formedness");
                 checkWellFormedness(xml);
@@ -52,17 +55,18 @@ public class ValidXMLValidator implements ConstraintValidator<ValidXML, byte[]> 
         return true;
     }
 
-    private void checkWellFormedness(InputStream xml) throws Exception {
+    private void checkWellFormedness(byte[] xml) throws Exception {
         XMLReader reader = XMLReaderFactory.createXMLReader();
-        reader.parse(new InputSource(xml));
+        InputStream xmlStream = new ByteArrayInputStream(xml);
+        reader.parse(new InputSource(xmlStream));
     }
 
-    private void checkValidity(InputStream xml, String xsd) throws Exception {
-        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        Source schemaSource = new StreamSource(xsd);
-        Schema schema = schemaFactory.newSchema(schemaSource);
-        Validator validator = schema.newValidator();
-        Source xmlSource = new StreamSource(xml);
+    private void checkValidity(byte[] xml) throws Exception {
+        String schemaLocation = schemaService.getSchemaLocation(xml);
+        log.debug("schemaLocation=" + schemaLocation);
+        Validator validator = schemaService.getSchemaValidator(schemaLocation);
+        InputStream xmlStream = new ByteArrayInputStream(xml);
+        Source xmlSource = new StreamSource(xmlStream);
         validator.validate(xmlSource);
     }
 
