64f811577cf37ad91093792f0bc0dddfad20a022
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2011-05-25 10:28:37 +0200

    implement GET for DOI API

--- src/main/java/org/datacite/mds/web/api/controller/DoiApiController.java
+++ src/main/java/org/datacite/mds/web/api/controller/DoiApiController.java
@@ -7,6 +7,7 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.datacite.mds.service.DoiService;
 import org.datacite.mds.service.HandleException;
+import org.datacite.mds.service.HandleService;
 import org.datacite.mds.service.SecurityException;
 import org.datacite.mds.web.api.ApiController;
 import org.datacite.mds.web.api.NotFoundException;
@@ -21,15 +22,31 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/*")
+@RequestMapping("/doi")
 public class DoiApiController implements ApiController {
 
     private static Logger log4j = Logger.getLogger(DoiApiController.class);
 
     @Autowired
     DoiService doiService;
-
-    @RequestMapping(value = "doi", method = { RequestMethod.PUT, RequestMethod.POST })
+    
+    @Autowired
+    HandleService handleService;
+    
+    @RequestMapping(value = "**", method = { RequestMethod.GET, RequestMethod.HEAD })
+    public ResponseEntity<String> resolve(HttpServletRequest httpRequest) throws HandleException, NotFoundException {
+        String doi = getDoiFromRequest(httpRequest);
+        String url = handleService.resolve(doi);
+        return new ResponseEntity<String>(url, HttpStatus.OK);
+    }
+    
+    private String getDoiFromRequest(HttpServletRequest request) {
+        String uri = request.getServletPath();
+        String doi = uri.replaceFirst("/doi/", "");
+        return doi;
+    }
+    
+    @RequestMapping(value = "", method = { RequestMethod.PUT, RequestMethod.POST })
     public ResponseEntity<String> createOrUpdate(@RequestBody String body,
             @RequestParam(required = false) Boolean testMode, HttpServletRequest httpRequest)
             throws ValidationException, HandleException, SecurityException, NotFoundException {
