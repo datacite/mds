152729aeace4c442b8c8818cab15388c91469772
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2011-08-02 15:35:00 +0200

    add tool NamespaceSetter

--- src/main/java/org/datacite/mds/tools/AbstractTool.java
+++ src/main/java/org/datacite/mds/tools/AbstractTool.java
@@ -1,11 +1,23 @@
 package org.datacite.mds.tools;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.stereotype.Component;
 
+@Component
 public abstract class AbstractTool {
     
     private static final String APPLICATION_CONTEXT = "META-INF/spring/applicationContext.xml";
+    
+    @Autowired
+    private LocalContainerEntityManagerFactoryBean emFactory;
+    
+    protected void disableValidation() {
+        emFactory.getJpaPropertyMap().put("javax.persistence.validation.mode", "ddl");
+        emFactory.afterPropertiesSet();
+    }
     
     @SuppressWarnings("unchecked")
     public static final void initAndRun(String[] args) {
---
+++ src/main/java/org/datacite/mds/tools/migration/NamespaceSetter.java
@@ -0,0 +1,73 @@
+package org.datacite.mds.tools.migration;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.datacite.mds.domain.Metadata;
+import org.datacite.mds.service.SchemaService;
+import org.datacite.mds.tools.AbstractTool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * This tool tries to set currently empty namespace field
+ */
+@Component
+@Transactional(propagation = Propagation.REQUIRES_NEW)
+public class NamespaceSetter extends AbstractTool {
+
+    private static Logger log = Logger.getLogger(NamespaceSetter.class);
+
+    @Autowired
+    SchemaService schemaService;
+
+    @PostConstruct
+    public void init() {
+        disableValidation();
+    }
+
+    @Override
+    public void run(String[] args) throws IOException {
+        System.out.println("Searching for metadata with empty namespace...");
+        List<Metadata> metadatas = Metadata.findAllMetadatas();
+        for (Metadata metadata : metadatas) {
+            if (StringUtils.isEmpty(metadata.getNamespace())) {
+                checkNamespace(metadata);
+            }
+        }
+    }
+
+    private void checkNamespace(Metadata metadata) {
+        log.debug("checking namespace: " + metadataToString(metadata));
+
+        byte[] xml = metadata.getXml();
+        String namespace = schemaService.getNamespace(xml);
+        if (namespace != null) {
+            setNamespace(metadata, namespace);
+        }
+    }
+
+    private void setNamespace(Metadata metadata, String namespace) {
+        metadata.setNamespace(namespace);
+        metadata.merge();
+
+        String msg = "set namespace: " + metadataToString(metadata) + " => " + namespace;
+        System.out.println(msg);
+        log.info(msg);
+    }
+
+    private String metadataToString(Metadata metadata) {
+        return metadata.getDataset().getDoi() + " #" + metadata.getMetadataVersion() + " (id=" + metadata.getId() + ")";
+    }
+
+    public static void main(String[] args) {
+        initAndRun(args);
+    }
+
+}
