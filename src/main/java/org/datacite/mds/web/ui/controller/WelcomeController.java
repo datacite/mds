89dd5dbb017fc0fbabe800e7563c688240fc9671
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2011-09-27 14:27:43 +0200

    move login pathway generation to UiUtils

--- src/main/java/org/datacite/mds/web/ui/controller/WelcomeController.java
+++ src/main/java/org/datacite/mds/web/ui/controller/WelcomeController.java
@@ -1,13 +1,8 @@
 package org.datacite.mds.web.ui.controller;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import javax.servlet.http.HttpSession;
 
-import org.datacite.mds.util.SecurityUtils;
 import org.datacite.mds.web.ui.UiUtils;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,22 +12,8 @@ public class WelcomeController {
     @RequestMapping
     public String welcome(HttpSession session) {
         UiUtils.refreshSymbolsForSwitchUser(session);
-        session.setAttribute("login_pathway", getLoginPathway());
+        UiUtils.setLoginPathwaySessionAttribute(session);
         return "index";
-    }
-
-    public List<String> getLoginPathway() {
-        LinkedList<String> pathway = new LinkedList<String>();
-
-        Authentication auth = SecurityUtils.getCurrentAuthentication();
-        
-        while(auth != null) {
-            pathway.addFirst(auth.getName());
-            auth = SecurityUtils.getOriginalUser(auth);
-        }
-        System.out.println(pathway);
-
-        return pathway;
     }
     
 
