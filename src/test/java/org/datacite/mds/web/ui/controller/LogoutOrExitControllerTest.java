7bad76654e551d733e6a1f4ec4f7d3d2a493525c
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2012-01-18 18:20:53 +0100

    fix tests and add missing for #213

--- src/test/java/org/datacite/mds/web/ui/controller/LogoutOrExitControllerTest.java
+++ src/test/java/org/datacite/mds/web/ui/controller/LogoutOrExitControllerTest.java
@@ -1,10 +1,14 @@
 package org.datacite.mds.web.ui.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import junit.framework.Assert;
 
 import org.datacite.mds.test.TestUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -24,8 +28,16 @@ public class LogoutOrExitControllerTest {
     @Test
     public void testNonSwitchedUser() {
         TestUtils.setUsernamePassword("foo", "bar");
-        String view = controller.logoutOrReturn();
+        HttpServletRequest request = makeHttpServletRequest(false);
+        String view = controller.logoutOrReturn(request);
         Assert.assertEquals("redirect:/resources/j_spring_security_logout", view);
+    }
+    
+    private HttpServletRequest makeHttpServletRequest(boolean sessionIsNew) {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpSession session = (MockHttpSession) request.getSession();
+        session.setNew(sessionIsNew);
+        return request;
     }
 
     @Test
@@ -36,7 +48,25 @@ public class LogoutOrExitControllerTest {
         Authentication switchedAuthentication = new TestingAuthenticationToken("foo", "bar", swichUserAuthority);
         SecurityContextHolder.getContext().setAuthentication(switchedAuthentication);
         
-        String view = controller.logoutOrReturn();
+        HttpServletRequest request = makeHttpServletRequest(false);
+        String view = controller.logoutOrReturn(request);
         Assert.assertEquals("redirect:/resources/j_spring_security_exit_user", view);
     }
+    
+    @Test
+    public void testNotLoggedIn() {
+        TestUtils.logout();
+        HttpServletRequest request = makeHttpServletRequest(false);
+        String view = controller.logoutOrReturn(request);
+        Assert.assertEquals("redirect:/", view);
+    }
+    
+    @Test
+    public void testWithNewSession() {
+        TestUtils.setUsernamePassword("foo", "bar");
+        HttpServletRequest request = makeHttpServletRequest(true);
+        String view = controller.logoutOrReturn(request);
+        Assert.assertEquals("redirect:/", view);
+    }
+    
 }
