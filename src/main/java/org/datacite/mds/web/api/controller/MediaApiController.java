3b9d223d0978e5c56544799dc419eed9af1870cd
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2012-03-14 18:55:19 +0100

    first implementation of media api (POST) (see #228)

--- src/main/java/org/datacite/mds/web/api/controller/MediaApiController.java
+++ src/main/java/org/datacite/mds/web/api/controller/MediaApiController.java
@@ -1,18 +1,22 @@
 package org.datacite.mds.web.api.controller;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.List;
+import java.util.Properties;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.datacite.mds.domain.AllocatorOrDatacentre;
+import org.datacite.mds.domain.Datacentre;
 import org.datacite.mds.domain.Dataset;
 import org.datacite.mds.domain.Media;
 import org.datacite.mds.service.SecurityException;
 import org.datacite.mds.util.SecurityUtils;
 import org.datacite.mds.util.Utils;
 import org.datacite.mds.web.api.ApiController;
+import org.datacite.mds.web.api.ApiUtils;
 import org.datacite.mds.web.api.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -60,8 +64,25 @@ public class MediaApiController implements ApiController {
 
     @RequestMapping(value = "*", method = { RequestMethod.POST })
     public ResponseEntity<String> post(@RequestBody String body, @RequestParam(required = false) Boolean testMode,
-            HttpServletRequest httpRequest) {
-        return null;
+            HttpServletRequest httpRequest) throws SecurityException, IOException {
+        String doi = getDoiFromRequest(httpRequest);
+        Datacentre datacentre = SecurityUtils.getCurrentDatacentre();
+        Dataset dataset = Dataset.findDatasetByDoi(doi);
+        
+        Properties props = new Properties();
+        props.load(new StringReader(body));
+        
+        for (String mediaType : props.stringPropertyNames()) {
+            String url = props.getProperty(mediaType);
+            Media media = new Media();
+            media.setDataset(dataset);
+            media.setMediaType(mediaType);
+            media.setUrl(url);
+            media.persist();
+        }
+
+        String message = ApiUtils.makeResponseMessage("OK", testMode);
+        return new ResponseEntity<String>(message, HttpStatus.OK);
     }
 
 }
