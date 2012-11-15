7ccc11b046f71f293738b563277161e637300aee
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2012-11-15 16:21:53 +0100

    check handle server on /status (closes #206)

--- src/main/java/org/datacite/mds/web/api/controller/StatusController.java
+++ src/main/java/org/datacite/mds/web/api/controller/StatusController.java
@@ -3,7 +3,10 @@ package org.datacite.mds.web.api.controller;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.datacite.mds.service.HandleException;
+import org.datacite.mds.service.HandleService;
 import org.datacite.mds.web.api.ApiController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,9 +18,13 @@ public class StatusController implements ApiController {
     @PersistenceContext
     EntityManager em;
     
+    @Autowired
+    HandleService handleService;
+    
     @RequestMapping(value = "/status")
-    public ResponseEntity<String> status() {
+    public ResponseEntity<String> status() throws HandleException {
         checkDatabaseConnection();
+        handleService.ping();
         return new ResponseEntity<String>("OK", null, HttpStatus.OK);
     }
     
