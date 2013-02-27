b79d5081cb4ad386978c46083f4b2e5fea3bc9d2
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2013-02-27 15:58:44 +0100

    set default list size 30

--- src/main/java/org/datacite/mds/web/ui/UiController.java
+++ src/main/java/org/datacite/mds/web/ui/UiController.java
@@ -4,6 +4,6 @@ package org.datacite.mds.web.ui;
  * Empty Interface to 'mark' all our UI controllers
  */
 public interface UiController {
-    public static int LIST_DEFAULT_SIZE = 50;
+    public static int LIST_DEFAULT_SIZE = 30;
     public static int LIST_MAX_SIZE = 100;
 }
