55e0cc308a66d0dd744931977ad7c1c09584adec
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2013-02-28 11:42:45 +0100

    add missing PrefixCreateModel

---
+++ src/main/java/org/datacite/mds/web/ui/model/PrefixCreateModel.java
@@ -0,0 +1,14 @@
+package org.datacite.mds.web.ui.model;
+
+public class PrefixCreateModel {
+
+    private String prefixes;
+
+    public String getPrefixes() {
+        return prefixes;
+    }
+
+    public void setPrefixes(String prefixes) {
+        this.prefixes = prefixes;
+    }
+}
