c4506723b4ac8504c202e428f823fc934c0adfd1
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2014-11-19 16:02:46 +0100

    fix NPE

--- src/main/java/org/datacite/mds/web/ui/controller/MediaController.java
+++ src/main/java/org/datacite/mds/web/ui/controller/MediaController.java
@@ -29,7 +29,8 @@ public class MediaController {
     @ModelAttribute("datasets")
     public Collection<Dataset> populateDatasets(@RequestParam(value = "dataset", required = false) Long datasetId) throws SecurityException {
         Dataset dataset = Dataset.findDataset(datasetId);
-        SecurityUtils.checkDatasetOwnership(dataset);
+        if (dataset != null)
+            SecurityUtils.checkDatasetOwnership(dataset);
         return Arrays.asList(dataset);
     }
     
