70c1ccd7ae8cdddf5c3b7d5ec8ff9f58dd12faf1
Sebastian Peters <sebastian.peters@tib.uni-hannover.de>
2014-11-19 14:54:45 +0100

    fix access rights for MetadataController (see #39)

--- src/main/java/org/datacite/mds/web/ui/controller/MetadataController.java
+++ src/main/java/org/datacite/mds/web/ui/controller/MetadataController.java
@@ -3,8 +3,13 @@ package org.datacite.mds.web.ui.controller;
 import java.util.Arrays;
 import java.util.Collection;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.datacite.mds.domain.Dataset;
 import org.datacite.mds.domain.Metadata;
+import org.datacite.mds.service.SecurityException;
+import org.datacite.mds.util.SecurityUtils;
 import org.datacite.mds.util.Utils;
 import org.datacite.mds.web.ui.UiController;
 import org.springframework.http.HttpHeaders;
@@ -14,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.roo.addon.web.mvc.controller.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -33,14 +39,29 @@ public class MetadataController implements UiController {
     }
 
     @ModelAttribute("datasets")
-    public Collection<Dataset> populateDatasets(@RequestParam(value = "dataset", required = false) Long datasetId) {
+    public Collection<Dataset> populateDatasets(@RequestParam(value = "dataset", required = false) Long datasetId) throws SecurityException {
         Dataset dataset = Dataset.findDataset(datasetId);
+        SecurityUtils.checkDatasetOwnership(dataset);
         return Arrays.asList(dataset);
     }
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public String create(@Valid Metadata metadata, BindingResult bindingResult, Model uiModel, HttpServletRequest httpServletRequest) throws SecurityException {
+        SecurityUtils.checkDatasetOwnership(metadata.getDataset());
+        if (bindingResult.hasErrors()) {
+            uiModel.addAttribute("metadata", metadata);
+            return "metadatas/create";
+        }
+        uiModel.asMap().clear();
+        metadata.persist();
+        return "redirect:/metadatas/" + encodeUrlPathSegment(metadata.getId().toString(), httpServletRequest);
+    }
+
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public String show(@PathVariable("id") Long id, Model model) {
+    public String show(@PathVariable("id") Long id, Model model) throws SecurityException {
         Metadata metadata = Metadata.findMetadata(id);
+        SecurityUtils.checkDatasetOwnership(metadata.getDataset());
         model.addAttribute("metadata", metadata);
         String prettyXml;
         try {
@@ -55,9 +76,10 @@ public class MetadataController implements UiController {
     }
 
     @RequestMapping(value = "/{id}", params = "raw", method = RequestMethod.GET)
-    public ResponseEntity<? extends Object> showRaw(@PathVariable("id") Long id) {
+    public ResponseEntity<? extends Object> showRaw(@PathVariable("id") Long id) throws SecurityException {
         HttpHeaders headers = new HttpHeaders();
         Metadata metadata = Metadata.findMetadata(id);
+        SecurityUtils.checkDatasetOwnership(metadata.getDataset());
         if (metadata == null) {
             return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
         }
